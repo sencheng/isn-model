d993949bc3ca1613b0f97da06719b767db8914bd
Mohammadreza Mohagheghi-Nejad <m.mohaghegh@gmail.com>
2020-11-04 22:43:15 +0100

    Improved visualization, added analysis file to analyze multiple simulation datasets, added a parameter file that involves parameter ranges for searching.

---
+++ searchParams.py
@@ -0,0 +1,21 @@
+################################################################################
+# -- Default set of parameters
+################################################################################
+
+import numpy as np; import pylab as pl; import time, sys, os
+import matplotlib
+from defaultParams import *
+
+fr_chg_factor = np.array([1])#np.arange(0.5, 1, .1)
+E_extra_stim_factor = np.arange(0.2, 1.1, 0.2)#np.array([0.8])
+EEconn_chg_factor = np.arange(0.9, 1.11, 0.1)#np.array([0.9])
+EIconn_chg_factor = np.arange(1.8, 2.21, 0.2)#np.array([2.0])
+IIconn_chg_factor = np.arange(1, 1.1, 0.2)
+bkg_chg_factor    = np.arange(1., 1.01, 0.05)
+
+EE_probchg_comb, EI_probchg_comb, II_condchg_comb, E_extra_comb, bkg_chg_comb = \
+    np.meshgrid(EEconn_chg_factor, EIconn_chg_factor, IIconn_chg_factor, E_extra_stim_factor, bkg_chg_factor)
+
+#pert_comb = pert_comb.flatten()[job_id::num_jobs]
+
+E_pert_frac = 1.0
