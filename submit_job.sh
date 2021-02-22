f2b9d73cabf5c2ccdc76cd9b1e8772f053308f01
Mohammadreza Mohagheghi Nejad <mohammadreza.mohagheghinejad@rub.de>
2021-02-22 08:02:03 +0100

    Added job submitting files.

---
+++ submit_job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -J ISN # A single job name for the array
+#SBATCH -n 4 # Number of cores
+#SBATCH -N 1 # All cores on one Node
+#SBATCH --mem 5000 # Memory request
+#SBATCH -t 1-00:00 # Maximum execution time (D-HH:MM)
+#SBATCH -o ./job_files/ISN_%A_%a.out # Standard output
+#SBATCH -e ./job_files/ISN_%A_%a.err # Standard error
+#SBATCH --mail-type FAIL
+#SBATCH --mail-user m.mohagheghi@ini.rub.de
+module restore cobel-spike
+#conda init
+source /home/mohagmnr/projects/CoBeL-spike/packages/set_vars.sh
+
+echo "running simulation for task ID ${SLURM_ARRAY_TASK_ID}"
+    
+python simulateNetworks.py ${SLURM_ARRAY_TASK_ID} 180
---
+++ submit_job_analysis.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -J ANALIZE_ISN # A single job name for the array
+#SBATCH -n 1 # Number of cores
+#SBATCH -N 1 # All cores on one Node
+#SBATCH --mem 5000 # Memory request
+#SBATCH -t 1-00:00 # Maximum execution time (D-HH:MM)
+#SBATCH -o ./job_files/ANALYZE_ISN_%A_%a.out # Standard output
+#SBATCH -e ./job_files/ANALYZE_ISN_%A_%a.err # Standard error
+#SBATCH --mail-type FAIL
+#SBATCH --mail-user m.mohagheghi@ini.rub.de
+module restore cobel-spike
+#conda init
+source /home/mohagmnr/projects/CoBeL-spike/packages/set_vars.sh
+
+echo "running simulation for task ID ${SLURM_ARRAY_TASK_ID}"
+    
+python figureNumInput_hpc.py ${SLURM_ARRAY_TASK_ID} 9
