f7ce734d07c984bbe1e3a4fdac666492cdb31cd6
Mohammadreza Mohagheghi Nejad <mohammadreza.mohagheghinejad@ruhr-uni-bochum.de>
2022-04-25 23:48:00 +0200

    extend the code for simulating the ISN tests.

---
+++ run_multiple_sims.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#source /local/mohammad/5HT2A/packages/nest-install/bin/nest_vars.sh
+log_dir="LOG"
+#rm -r $log_dir
+mkdir $log_dir
+
+for sim in {0..7}
+do
+    echo $sim
+    rm "$log_dir/$sim.log" "$log_dir/$sim.err"
+    python simulateNetworks_hpc.py $sim $1>"$log_dir/$sim.log" 2>"$log_dir/$sim.err" &
+done
